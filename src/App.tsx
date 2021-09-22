4b504083feb477df26bac4251cb5ecd9f478ac22
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-22 14:20:28 +0100

    work :wrench:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import {Container, Input, Button, Form, FormButton, Message} from "semantic-ui-react";
+import {FormEvent, useState} from "react";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-    </div>
-  );
+
+    const [shop, setShop] = useState<string>("");
+    const [amount, setAmount] = useState<any>(0.00);
+    const [date, setDate] = useState<string>("--");
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        console.log("submitting data...");
+    }
+
+    return (
+        <Container className={"container"}>
+            <div className={"info"}>
+                <h1>Finance Sheets</h1>
+                <p>Shop: {shop}, Amount: {amount}, Date: {date}</p>
+            </div>
+            <Form className={"main-form"} onSubmit={(e) => {handleSubmit(e)}}>
+                <Form.Input fluid label={"Shop"} type={"text"} placeholder={"Amazon"} onChange={(e) => setShop(e.target.value)}/>
+                <Form.Input fluid label={"Amount"} type={"number"} placeholder={34.99} onChange={(e) => setAmount(e.target.value)} min={0.00} step={0.01}/>
+                <Form.Input fluid label={"Date"} type={"date"} onChange={(e) => setDate(e.target.value)}/>
+                <FormButton fluid type={"submit"} color={"blue"}>Submit</FormButton>
+            </Form>
+        </Container>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css'
+import './styles/index.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
